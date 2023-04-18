c0e5dd95a8a0a8ec224fe119840692957811dac7
BambicoIsaac <isaacbambico@gmail.com>
2023-04-18 11:18:23 +0800

    Revert "Put to-do-list in table form"
    This reverts commit efe623c33b30b8410c1c15c29f139eb7c595379d.

--- resources/views/todo/home.blade.php
+++ resources/views/todo/home.blade.php
@@ -11,33 +11,24 @@
     
     <a href="/todo/create">Create New Todo</a>
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Task</th>
-            <th scope="col">Due Date</th>
-            <th scope="col">Functions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($todos as $todo)
-            <tr>
-            <th scope="row">{{$todo -> id}}</th>
-            <td>{{ $todo -> title }}</td>
-            <td>{{ $todo -> due_date }}</td>
-            <td>
+    <ul>
+        @foreach ($todos as $todo)
+            <li>
+                {{ $todo -> id }}.
+                <b>
+                {{ $todo -> title }}
+                </b>
+                {{ $todo -> due_date}}
+
                 <a href="/todo/update/{{ $todo->id }}" style="color:green;">
                     Update
                 </a>
+
                 <a href="/todo/delete/{{ $todo->id }}" style="color:red;">
                     Delete
                 </a>
-            </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
