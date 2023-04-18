5cd3fe60c601e1b211f5b53b2ba870df3295e690
BambicoIsaac <isaacbambico@gmail.com>
2023-04-18 11:43:40 +0800

    Put to-do-list in table form

--- resources/views/todo/home.blade.php
+++ resources/views/todo/home.blade.php
@@ -4,22 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Todo - Home</title>
 </head>
 <body>
-    <h1>Todo - Home</h1>
+    <h1 style="text-align: center;">Todo - Home</h1>
+    <br><br>
     
     <a href="/todo/create">Create New Todo</a>
 
-    <ul>
-        @foreach ($todos as $todo)
-            <li>
-                {{ $todo -> id }}.
-                <b>
-                {{ $todo -> title }}
-                </b>
-                {{ $todo -> due_date}}
-
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Task</th>
+            <th scope="col">Due Date</th>
+            <th scope="col">Operations</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($todos as $todo)
+            <tr>
+            <th scope="row">{{ $todo -> id }}.</th>
+            <td><b>{{ $todo -> title }}</b></td>
+            <td>{{ $todo -> due_date}}</td>
+            <td>                
                 <a href="/todo/update/{{ $todo->id }}" style="color:green;">
                     Update
                 </a>
@@ -27,8 +36,12 @@
                 <a href="/todo/delete/{{ $todo->id }}" style="color:red;">
                     Delete
                 </a>
-            </li>
-        @endforeach
-    </ul>
+            </td>
+            </tr>
+            @endforeach     
+
+        </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
